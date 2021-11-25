1e90c71f0cafcd1120041bfa5741b69e1617d6f9
Nikolay Gagara <ggn.f763@gmail.com>
2021-11-25 07:23:48 +0300

    Updates

--- src.3ed60ec9.js
+++ src.3ed60ec9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./styles.scss"),console.log("hello world!");
 },{"./styles.scss":"I4sy"}]},{},["Focm"], null)
-//# sourceMappingURL=src.ce4b59ea.js.map
+//# sourceMappingURL=src.3ed60ec9.js.map
